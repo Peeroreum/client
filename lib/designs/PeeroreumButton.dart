9d6664589e3c5509ec3162baa2cf37b32078f234
egg-z1 <kimfreedom25@gmail.com>
2023-11-28 19:22:36 +0900

    feat: 드롭다운버튼 디자인시스템(#60)

--- lib/designs/PeeroreumButton.dart
+++ lib/designs/PeeroreumButton.dart
@@ -1,31 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'PeeroreumColor.dart';
 
 class PeeroreumButton<T> extends StatelessWidget {
   final List<T> items;
   final T? value;
   final void Function(T?) onChanged;
   final String hintText;
+  final double width;
 
-  PeeroreumButton({
-    required this.items,
-    required this.value,
-    required this.onChanged,
-    required this.hintText,
-  });
+  PeeroreumButton(
+      {required this.items,
+      required this.value,
+      required this.onChanged,
+      required this.hintText,
+      this.width = 78});
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<T>(
-      value: value,
-      items: items.map((item) {
-        return DropdownMenuItem<T>(
-          value: item,
-          child: Text(item.toString()),
-        );
-      }).toList(),
-      onChanged: onChanged,
-      hint: Text(hintText),
-      underline: SizedBox.shrink(), // 밑줄 숨기기
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        border: Border.all(color: PeeroreumColor.gray[200]!, width: 1),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: SizedBox(
+        height: 40,
+        width: width,
+        child: DropdownButton<T>(
+          isExpanded: true,
+          padding: EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+          value: value,
+          iconSize: 18,
+          icon: Container(
+            margin: EdgeInsets.only(left: 7),
+            child: SvgPicture.asset('assets/icons/down.svg',
+                color: PeeroreumColor.gray[600]),
+          ),
+          items: items.map((e) {
+            return DropdownMenuItem<T>(
+              value: e,
+              child: Text(
+                e.toString(),
+                style: TextStyle(
+                  color: PeeroreumColor.black,
+                  fontFamily: 'Pretendard',
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            );
+          }).toList(),
+          onChanged: onChanged,
+          hint: Text(
+            hintText,
+            style: TextStyle(
+              color: PeeroreumColor.gray[600],
+              fontFamily: 'Pretendard',
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          underline: SizedBox.shrink(), // 밑줄 숨기기
+        ),
+      ),
     );
   }
 }
