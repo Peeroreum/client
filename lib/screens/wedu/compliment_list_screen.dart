368e96fd9628b11627379d4831c02e8f025c702e
egg-z1 <kimfreedom25@gmail.com>
2024-01-31 20:17:02 +0900

    feat: 친구 클릭시 프로필로 이동 calender, 달성 및 미달성 전체보기 (#251)

--- lib/screens/wedu/compliment_list_screen.dart
+++ lib/screens/wedu/compliment_list_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:peeroreum_client/designs/PeeroreumColor.dart';
+import 'package:peeroreum_client/screens/mypage/mypage_profile.dart';
 
 class ComplimentList extends StatefulWidget {
   @override
@@ -9,101 +11,111 @@ class ComplimentList extends StatefulWidget {
 
 class _ComplimentListState extends State<ComplimentList> {
   List<dynamic> successList = [];
+  bool am_i = false;
+  var mynickname;
+
+  Future<void> fetchDatas() async {
+    mynickname = await FlutterSecureStorage().read(key: "nickname");
+  }
+
   @override
   Widget build(BuildContext context) {
     successList = ModalRoute.of(context)!.settings.arguments as List<dynamic>;
-
-    return Scaffold(
-        backgroundColor: PeeroreumColor.white,
-        appBar: AppBar(
-          backgroundColor: PeeroreumColor.white,
-          shadowColor: Colors.transparent,
-          surfaceTintColor: Colors.transparent,
-          elevation: 0,
-          leading: IconButton(
-            icon: SvgPicture.asset(
-              'assets/icons/x.svg',
-              color: PeeroreumColor.gray[800],
-              width: 18,
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          title: Text(
-            '달성',
-            style: TextStyle(
-                fontFamily: 'Pretendard',
-                fontWeight: FontWeight.w500,
-                fontSize: 20,
-                color: PeeroreumColor.black),
-          ),
-          centerTitle: true,
-          actions: [
-            IconButton(
-                onPressed: () {},
+    return FutureBuilder<void>(
+        future: fetchDatas(),
+        builder: (context, snapshot) {
+          return Scaffold(
+            backgroundColor: PeeroreumColor.white,
+            appBar: AppBar(
+              backgroundColor: PeeroreumColor.white,
+              shadowColor: Colors.transparent,
+              surfaceTintColor: Colors.transparent,
+              elevation: 0,
+              leading: IconButton(
                 icon: SvgPicture.asset(
-                  'assets/icons/icon_dots_mono.svg',
+                  'assets/icons/x.svg',
                   color: PeeroreumColor.gray[800],
-                  width: 24,
-                ))
-          ],
-        ),
-        body: Container(
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(vertical:13, horizontal:20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
+                  width: 18,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              title: Text(
+                '달성',
+                style: TextStyle(
+                    fontFamily: 'Pretendard',
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: PeeroreumColor.black),
+              ),
+              centerTitle: true,
+              actions: [
+                IconButton(
+                    onPressed: () {},
+                    icon: SvgPicture.asset(
+                      'assets/icons/icon_dots_mono.svg',
+                      color: PeeroreumColor.gray[800],
+                      width: 24,
+                    ))
+              ],
+            ),
+            body: Container(
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 13, horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(
-                          '전체',
-                          style: TextStyle(
-                              fontFamily: 'Pretendard',
-                              fontWeight: FontWeight.w500,
-                              fontSize: 14,
-                              color: PeeroreumColor.gray[500]),
-                        ),
-                        SizedBox(
-                          width: 4,
-                        ),
-                        Text(
-                          '${successList.length}',
-                          style: TextStyle(
-                              fontFamily: 'Pretendard',
-                              fontWeight: FontWeight.w500,
-                              fontSize: 14,
-                              color: PeeroreumColor.gray[500]),
-                        ),
-                        SizedBox(
-                          width: 2,
-                        ),
-                        Text(
-                          '명',
-                          style: TextStyle(
-                              fontFamily: 'Pretendard',
-                              fontWeight: FontWeight.w500,
-                              fontSize: 14,
-                              color: PeeroreumColor.gray[500]),
+                        Row(
+                          children: [
+                            Text(
+                              '전체',
+                              style: TextStyle(
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14,
+                                  color: PeeroreumColor.gray[500]),
+                            ),
+                            SizedBox(
+                              width: 4,
+                            ),
+                            Text(
+                              '${successList.length}',
+                              style: TextStyle(
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14,
+                                  color: PeeroreumColor.gray[500]),
+                            ),
+                            SizedBox(
+                              width: 2,
+                            ),
+                            Text(
+                              '명',
+                              style: TextStyle(
+                                  fontFamily: 'Pretendard',
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 14,
+                                  color: PeeroreumColor.gray[500]),
+                            ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                  Divider(
+                    color: PeeroreumColor.gray[100],
+                    thickness: 1,
+                    height: 8,
+                  ),
+                  okList()
+                ],
               ),
-              Divider(
-                color: PeeroreumColor.gray[100],
-                thickness: 1,
-                height: 8,
-              ),
-              okList()
-            ],
-          ),
-        ),
-        );
+            ),
+          );
+        });
   }
 
   okList() {
@@ -111,53 +123,65 @@ class _ComplimentListState extends State<ComplimentList> {
         // width: MediaQuery.of(context).size.width,
         // height: MediaQuery.of(context).size.height-140,
         child: ListView.separated(
-          scrollDirection: Axis.vertical,
+            scrollDirection: Axis.vertical,
             itemBuilder: (BuildContext context, int index) {
-              return Container(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-                child: Row(
-                  children: [
-                    Container(
-                      width: 48,
-                      height: 48,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                            width: 2,
-                            color: PeeroreumColor
-                                .gradeColor[successList[index]['grade']]!),
-                      ),
-                      child: Container(
-                        height: 44,
-                        width: 44,
+              return GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onTap: () {
+                  if (mynickname == successList[index]['nickname']) {
+                    am_i = true;
+                  }
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) =>
+                          MyPageProfile(successList[index]['nickname'], am_i)));
+                },
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 48,
+                        height: 48,
                         decoration: BoxDecoration(
                           shape: BoxShape.circle,
                           border: Border.all(
-                            width: 1,
-                            color: PeeroreumColor.white,
+                              width: 2,
+                              color: PeeroreumColor
+                                  .gradeColor[successList[index]['grade']]!),
+                        ),
+                        child: Container(
+                          height: 44,
+                          width: 44,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 1,
+                              color: PeeroreumColor.white,
+                            ),
+                            image: successList[index]["profileImage"] != null
+                                ? DecorationImage(
+                                    image: NetworkImage(
+                                        successList[index]["profileImage"]),
+                                    fit: BoxFit.cover)
+                                : DecorationImage(
+                                    image:
+                                        AssetImage('assets/images/user.jpg')),
                           ),
-                          image: successList[index]["profileImage"] != null
-                              ? DecorationImage(
-                                  image: NetworkImage(
-                                      successList[index]["profileImage"]),
-                                  fit: BoxFit.cover)
-                              : DecorationImage(
-                                  image: AssetImage('assets/images/user.jpg')),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      successList[index]['nickname'],
-                      style: TextStyle(
-                          fontFamily: 'Pretendard',
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14,
-                          color: PeeroreumColor.gray[800]),
-                    )
-                  ],
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        successList[index]['nickname'],
+                        style: TextStyle(
+                            fontFamily: 'Pretendard',
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                            color: PeeroreumColor.gray[800]),
+                      )
+                    ],
+                  ),
                 ),
               );
             },
