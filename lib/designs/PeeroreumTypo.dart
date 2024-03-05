f89937d0bf34d0b4f7b7a7f060d3ea68454adc76
veronica2550 <minkyoung2550@gmail.com>
2024-03-05 23:49:50 +0900

    feat: 피어오름 타이포 시스템 overflow 속성 추가(#301)

--- lib/designs/PeeroreumTypo.dart
+++ lib/designs/PeeroreumTypo.dart
@@ -7,11 +7,13 @@ class T1_24px extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const T1_24px({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -24,6 +26,7 @@ class T1_24px extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 24,
           fontWeight: FontWeight.w600),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -32,11 +35,13 @@ class T2_20px extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const T2_20px({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -49,6 +54,7 @@ class T2_20px extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 20,
           fontWeight: FontWeight.w600),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -57,11 +63,13 @@ class T3_18px extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const T3_18px({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -74,6 +82,7 @@ class T3_18px extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 18,
           fontWeight: FontWeight.w600),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -82,11 +91,13 @@ class T4_16px extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const T4_16px({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -99,7 +110,7 @@ class T4_16px extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 16,
           fontWeight: FontWeight.w600),
-      overflow: TextOverflow.ellipsis,
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -108,11 +119,13 @@ class T5_14px extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const T5_14px({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -125,6 +138,7 @@ class T5_14px extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 14,
           fontWeight: FontWeight.w600),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -135,11 +149,13 @@ class B1_24px_M extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const B1_24px_M({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -152,6 +168,7 @@ class B1_24px_M extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 24,
           fontWeight: FontWeight.w500),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -160,11 +177,13 @@ class B2_20px_M extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const B2_20px_M({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -177,6 +196,7 @@ class B2_20px_M extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 20,
           fontWeight: FontWeight.w500),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -185,11 +205,13 @@ class B3_18px_M extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const B3_18px_M({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -202,6 +224,7 @@ class B3_18px_M extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 18,
           fontWeight: FontWeight.w500),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -210,11 +233,13 @@ class B4_14px_M extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const B4_14px_M({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -227,7 +252,7 @@ class B4_14px_M extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 14,
           fontWeight: FontWeight.w500),
-      overflow: TextOverflow.ellipsis,
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -238,11 +263,13 @@ class B1_24px_R extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const B1_24px_R({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -255,6 +282,7 @@ class B1_24px_R extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 24,
           fontWeight: FontWeight.w400),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -263,11 +291,13 @@ class B2_20px_R extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const B2_20px_R({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -280,6 +310,7 @@ class B2_20px_R extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 20,
           fontWeight: FontWeight.w400),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -288,11 +319,13 @@ class B3_18px_R extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const B3_18px_R({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -305,6 +338,7 @@ class B3_18px_R extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 18,
           fontWeight: FontWeight.w400),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -313,11 +347,13 @@ class B4_14px_R extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const B4_14px_R({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -331,7 +367,7 @@ class B4_14px_R extends StatelessWidget {
         fontSize: 14,
         fontWeight: FontWeight.w400,
       ),
-      overflow: TextOverflow.ellipsis,
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -342,11 +378,13 @@ class C1_12px_Sb extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const C1_12px_Sb({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -359,6 +397,7 @@ class C1_12px_Sb extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 12,
           fontWeight: FontWeight.w600),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -367,11 +406,13 @@ class C1_12px_M extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const C1_12px_M({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -384,6 +425,7 @@ class C1_12px_M extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 12,
           fontWeight: FontWeight.w500),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -392,11 +434,13 @@ class C1_12px_R extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const C1_12px_R({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -409,6 +453,7 @@ class C1_12px_R extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 12,
           fontWeight: FontWeight.w400),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -417,11 +462,13 @@ class C2_10px_Sb extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const C2_10px_Sb({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -434,6 +481,7 @@ class C2_10px_Sb extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 10,
           fontWeight: FontWeight.w600),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -442,11 +490,13 @@ class C2_10px_M extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const C2_10px_M({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -459,6 +509,7 @@ class C2_10px_M extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 10,
           fontWeight: FontWeight.w500),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
@@ -467,11 +518,13 @@ class C2_10px_R extends StatelessWidget {
   final String text;
   final TextStyle style;
   final Color? color;
+  final TextOverflow? overflow;
 
   const C2_10px_R({
     Key? key,
     required this.text,
     this.color,
+    this.overflow,
     this.style = const TextStyle(),
   }) : super(key: key);
 
@@ -484,6 +537,7 @@ class C2_10px_R extends StatelessWidget {
           fontFamily: 'Pretendard',
           fontSize: 10,
           fontWeight: FontWeight.w400),
+      overflow: overflow ?? TextOverflow.clip,
     );
   }
 }
