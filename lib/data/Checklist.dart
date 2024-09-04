94a66a3ab5979ccb2a97a1158de3a97d5bb2c5de
veronica2550 <minkyoung2550@gmail.com>
2024-09-04 18:31:36 +0900

    Fix: 칭찬 독려 이슈 해결(#398)

--- lib/data/Checklist.dart
+++ lib/data/Checklist.dart
@@ -4,14 +4,14 @@ import 'package:shared_preferences/shared_preferences.dart';
 class CheckComplimentList{
   static Future<void> setComplimentCheck(int id, List<Map<String, String>> memberList) async{
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    String _checklistKey = "room_$id";
+    String _checklistKey = "compliment_room_$id";
     List<String> memberStringList = memberList.map((item) => jsonEncode(item)).toList();
     prefs.setStringList(_checklistKey, memberStringList);
   }
 
   static Future<List<Map<String, String>>?> getComplimentCheck(int id) async{
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    String _checklistKey = "room_$id";
+    String _checklistKey = "compliment_room_$id";
     List<String> memberStringList = prefs.getStringList(_checklistKey) ?? [];
     return memberStringList.map((item){
       final decodedItem = jsonDecode(item) as Map;
@@ -23,14 +23,14 @@ class CheckComplimentList{
 class CheckEncouragementList{
   static Future<void> setEncouragementCheck(int id, List<Map<String, String>> memberList) async{
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    String _checklistKey = "room_$id";
+    String _checklistKey = "encouragement_room_$id";
     List<String> memberStringList = memberList.map((item) => jsonEncode(item)).toList();
     prefs.setStringList(_checklistKey, memberStringList);
   }
 
   static Future<List<Map<String, String>>?> getEncouragementCheck(int id) async{
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    String _checklistKey = "room_$id";
+    String _checklistKey = "encouragement_room_$id";
     List<String> memberStringList = prefs.getStringList(_checklistKey) ?? [];
     return memberStringList.map((item){
       final decodedItem = jsonDecode(item) as Map;
